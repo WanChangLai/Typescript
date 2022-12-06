6a669575e71cf51a23462437f8e6e2dcdec7b643
Wan Chang Lai <wanchang.lai@live.com>
2022-12-06 20:12:03 +0800

    Generics

--- basic.ts
+++ basic.ts
@@ -51,3 +51,19 @@ function add(a: number, b: number) {
 function printOutput(value: any) {
   console.log(value);
 }
+
+//Generics
+
+//newArray contain the old array and value
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+//this two variable construct by insertatbeginning will have the type T-- make it flexible
+const updatedArray = insertAtBeginning(demoArray, -1); //[-1,1,2,3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+//updatedArray[0].split("");
