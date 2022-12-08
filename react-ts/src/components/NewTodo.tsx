dc8f42bb77e3d67b221edba3f87dc72de250bcb6
Wan Chang Lai <wanchang.lai@live.com>
2022-12-08 14:53:55 +0800

    useRef with form

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,13 +1,26 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   // event object type
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    //? try to access value and if it suceeds, it stored in enteredText
+    // if that fails, it stored null
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      //throw an error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo test</label>
-      <input type="text" id="text"></input>
+      <input type="text" id="text" ref={todoTextInputRef}></input>
       <button>Add Todo</button>
     </form>
   );
