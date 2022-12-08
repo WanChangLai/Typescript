00dd85965266d68193b0adff84c6564aa6075c4e
Wan Chang Lai <wanchang.lai@live.com>
2022-12-08 14:00:43 +0800

    Add TodoItem

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 
 //fc stands for functional component
@@ -7,7 +8,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
